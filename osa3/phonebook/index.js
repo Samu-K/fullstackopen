51f46cde2b314bef52c1cea4ff9286302665ad2a
Samu-K <samu.kaarlela@gmail.com>
2022-11-14 00:28:25 +0200

    Finished part3c

--- osa3/phonebook/index.js
+++ osa3/phonebook/index.js
@@ -5,7 +5,6 @@ const morgan = require("morgan")
 const cors = require("cors")
 
 const Person = require("./models/person")
-const { json } = require('express')
 
 const app = express()
 
@@ -13,10 +12,82 @@ morgan.token("body",req => {
     return JSON.stringify(req.body)
 })
 
+app.use(express.static("build"))
 app.use(express.json())
 app.use(cors())
 app.use(morgan(":method :url :status :response-time :body"))
-app.use(express.static("build"))
+
+app.post("/api/persons", (req,resp) => {
+    const rbody = req.body
+
+    if (rbody.name==null || rbody.number==null) {
+        console.log("content missing error")
+        return resp.status(400).json({
+            error: "content missing"
+        })
+    } else if (isDuplicate(Person,rbody.name)) {
+        return resp.send(400).json({
+            error: "name duplicate error"
+        })
+    } else {
+        addPerson(Person,rbody, resp)
+    }
+})
+
+app.get("/", (req,resp) => {
+    // placeholder if frontend build fails
+    resp.send("<h1>Front page</h1>")
+})
+
+app.get("/api/persons/",(req,resp) => {
+    Person.find({}).then( persons => {
+        resp.json(persons)
+    })
+})
+
+app.get("/api/persons/:id",(req,resp) => {
+    Person.findById(req.params.id).then(person => {
+        if (person) {
+            resp.json(person)
+        } else {
+            resp.status(404).end()
+        }}
+    )
+    .catch(error => next(error))
+})
+
+app.get("/info",(req,resp) => {
+    const date = new Date()
+    Person.count({},(err,pplnum)=> {
+        resp.send(`<p>Phonebook has info for ${pplnum}</p> <p>${date}</p>`)
+    })
+})
+
+app.delete("/api/persons/:id", (req,resp) => {
+    Person.findOneAndDelete({"id":req.params.id})
+    .then(result => resp.status(204).end())
+    .catch(error => next(error))
+})
+
+const unknownEndpoint = (req,resp) => {
+    resp.status(404).send({error: "unknown endpoint"})
+}
+
+app.use(unknownEndpoint)
+
+const errorHandler = (error, req, resp, next) => {
+    console.error(error.message)
+
+    if (error.name === "CastError") {
+        return resp.status(400).send({error: "malformatted id"})
+    } else if (error.name === "ValidationError") {
+        return resp.status(400).json({error: error.message})
+    }
+
+    next(error)
+}
+
+app.use(errorHandler)
 
 function isDuplicate(Person,pname) {
     Person.countDocuments({"name":pname},{limit:1},(err,num) => {
@@ -40,64 +111,16 @@ function addPerson(Person, rbody,resp) {
             id: mem.id+1,
         })
         
-        person.save().then(savedPerson => {
+        person.save()
+        .then(savedPerson => {
             resp.json(savedPerson)
         })
+        .catch(error => next(error))
         
     })
 }
 
-app.get("/", (req,resp) => {
-    // placeholder if frontend build fails
-    resp.send("<h1>Front page</h1>")
-})
-
-app.get("/api/persons/",(req,resp) => {
-    Person.find({}).then( persons => {
-        resp.json(persons)
-    })
-})
-
-app.get("/api/persons/:id",(req,resp) => {
-    Person.findById(req.params.id).then(person => {
-        resp.json(person)
-    })
-})
-
-app.get("/info",(req,resp) => {
-    const date = new Date()
-    Person.count({},(err,pplnum)=> {
-        resp.send(`<p>Phonebook has info for ${pplnum}</p> <p>${date}</p>`)
-    })
-})
-
-app.post("/api/persons", (req,resp) => {
-    const rbody = req.body
-
-    if (rbody.name==null || rbody.number==null) {
-        console.log("content missing error")
-        return resp.status(400).json({
-            error: "content missing"
-        })
-    } else if (isDuplicate(Person,rbody.name)) {
-        return resp.send(400).json({
-            error: "name duplicate error"
-        })
-    } else {
-        addPerson(Person,rbody, resp)
-    }
-})
-
-app.delete("/api/persons/:id", (req,resp) => {
-    Person.findOneAndDelete({"id":req.params.id})
-    .catch(error => {
-        console.log(`Error: ${error}`)
-    })
-
-    resp.status(204).end()
-})
-
 const PORT = process.env.PORT || 3001
-app.listen(PORT, ()=> {
+app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
